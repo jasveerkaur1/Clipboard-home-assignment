77d4cf763b86a8c53b52868f6005fc47efda1370
jasveerkaur1 <43728043+jasveerkaur1@users.noreply.github.com>
2022-11-27 10:52:31 -0500

    Update dbk.js

--- dpk.js
+++ dpk.js
@@ -1,28 +1,32 @@
 const crypto = require("crypto");
 
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const OBJECT_TYPE = 'object';
+const STRING_TYPE = 'string';
+
+const HASH_ALGORITHM = 'sha3-512';
+const HASH_FORMAT = 'hex';
+
+
+const stringfyInput = (input) => typeof input === STRING_TYPE ? input : JSON.stringify(input);
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  /** Handling corner case, when we have event as undefined */
+  if(!event) return TRIVIAL_PARTITION_KEY;
+  
+  /** case - when we have event.partitionKey  */
+  if(typeof event === OBJECT_TYPE && event.partitionKey) return event.partitionKey
+
+  const generatedHash = stringfyInput(crypto.createHash(HASH_ALGORITHM).update(stringfyInput(event)).digest(HASH_FORMAT));
+
+  if (generatedHash.length > MAX_PARTITION_KEY_LENGTH) {
+    return crypto.createHash(HASH_ALGORITHM).update(generatedHash).digest(HASH_FORMAT);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return generatedHash;
 };
+
+
