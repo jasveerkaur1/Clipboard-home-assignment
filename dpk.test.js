8aeba4bff8ddfa1493dce7688c68f06fce60392c
jasveerkaur1 <43728043+jasveerkaur1@users.noreply.github.com>
2022-11-27 10:53:07 -0500

    Adding test cases

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,43 @@
+const crypto = require("crypto");
+
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it(`If input is 100, expecting corresponding hash value`, () => {
+    const input = 100;
+    const trivialKey = deterministicPartitionKey(input);
+    const generatedHash = crypto.createHash('sha3-512').update(JSON.stringify(input)).digest('hex');
+    expect(trivialKey).toBe(generatedHash);
+  });
+
+  it(`If input is '100', expecting corresponding hash value`, () => {
+    const input = '100';
+    const trivialKey = deterministicPartitionKey(input);
+    const generatedHash = crypto.createHash('sha3-512').update(input).digest('hex');
+    expect(trivialKey).toBe(generatedHash);
+  });
+
+  it(`If input is 'null', expecting corresponding hash value`, () => {
+    const input = 'null';
+    const trivialKey = deterministicPartitionKey(input);
+    const generatedHash = crypto.createHash('sha3-512').update(input).digest('hex');
+    expect(trivialKey).toBe(generatedHash);
+  });
+
+  it(`If input is { partitionKey: 100 }, expecting corresponding hash value`, () => {
+    const input = { partitionKey: 100 };
+    const trivialKey = deterministicPartitionKey(input);
+    expect(trivialKey).toBe(input.partitionKey);
+  });
+
+  it(`If input is {partitionKey: {}}, expecting corresponding hash value`, () => {
+    const input = {partitionKey: {}};
+    const trivialKey = deterministicPartitionKey(input);
+    expect(trivialKey).toBe(input.partitionKey);
   });
 });
